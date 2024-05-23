59d2823765a3f622909ef11a1d0f9f4c054edc8a
Deshi Rahim <nawalsrahim@gmail.com>
2024-05-22 21:46:44 -0700

    ✨ Add spelling check to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -514,3 +514,7 @@ cmp.setup {
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+-- [[ Spell Check ]]
+vim.opt.spelllang = 'en_us'
+vim.opt.spell = true
